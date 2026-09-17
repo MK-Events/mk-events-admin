89488e3f65afe848a9c8c129ebc5ad763533a975
Abhijeet Kumar <abhijeetk829@gmail.com>
2026-09-18 05:23:13 +0530

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@mk": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_URL ?? "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
